2ca7ca2d4b2d909e838d26620ace1098e518bd3f
Rohit Vishwakarma <rohit8840@gmail.com>
2024-02-05 21:58:07 +0530

    added more code

--- index.js
+++ index.js
@@ -6,13 +6,30 @@ const app=express();
 // let port=3000;
 let port=8080;
 
+
+
 app.listen(port,()=>{
 
-console.log(`app is listening on port   ${port}`);
+console.log(`app is listening on port   ${port}`); 
 });
  
 
+app.use((req,res)=>{
+    // console.log(req);
+    console.log(" new incoming request  ");
+    // res.send("this is a basic response");
 
+// res.send({
+//     name:"fruit",
+//     color:"red"
+// })
+
+
+let code="<h1>fruits <ul><li>orange</li><li>apple</li></ul></h1>";
+res.send(code);
+
+
+});
 
 
 
