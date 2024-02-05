2c32b6efe85576c5a9a3e8c1988bc5b07dbf3905
Rohit Vishwakarma <rohit8840@gmail.com>
2024-02-05 20:53:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+
+// 
+
+// let port=3000;
+let port=8080;
+
+app.listen(port,()=>{
+
+console.log(`app is listening on port   ${port}`);
+});
+ 
+
+
+
+
+
+
+
+
+
+
