990da1a58a7bb567842802118a28098d7203ff7d
Rohit Vishwakarma <rohit8840@gmail.com>
2024-02-06 18:29:34 +0530

    added more files

--- index.js
+++ index.js
@@ -14,22 +14,65 @@ console.log(`app is listening on port   ${port}`);
 });
  
 
-app.use((req,res)=>{
-    // console.log(req);
-    console.log(" new incoming request  ");
-    // res.send("this is a basic response");
+app.get("/",(req,res)=>{
+    res.send("you contacted root path");
+});
 
-// res.send({
-//     name:"fruit",
-//     color:"red"
-// })
+app.get("/help",(req,res)=>{
+    res.send("i am help you contacted me hi");
+});
+
+app.get("/about",(req,res)=>{
+    res.send(" you contacted about my path ");
+});
+
+// after all defined use that 
+// app.get("*",(req,res)=>{
+//     res.send("this path does not exist demo text   ");
+// });
 
 
-let code="<h1>fruits <ul><li>orange</li><li>apple</li></ul></h1>";
-res.send(code);
 
+
+app.post("/",(req,res)=>{
+    res.send("you contacted post");
+});
+
+app.get("/:username/:id",(req,res)=>{
+    
+let {username,id}=req.params;
+
+    // console.log(req.params);
+    // res.send("hello i am root ");
+    // res.send(`welcome to the page of @${username}`);
+
+let htmlstr=`<h1>welcome to the page of @${username}</h1>`
+
+res.send(htmlstr);
 
 });
+
+
+
+
+
+
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log(" new incoming request  ");
+//     // res.send("this is a basic response");
+
+// // res.send({
+// //     name:"fruit",
+// //     color:"red"
+// // })
+
+
+// let code="<h1>fruits <ul><li>orange</li><li>apple</li></ul></h1>";
+// res.send(code);
+
+
+// });
 
 
 
